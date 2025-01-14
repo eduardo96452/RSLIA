e24802d3351f90b9d1b3c1f5ce5bbec62cf1f0eb
Eduardo <86896508+Edua7d0@users.noreply.github.com>
2025-01-14 11:42:10 -0500

    modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,33 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     // Tus componentes
   ],
+  
   imports: [
+    FormsModule,
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,  // Requerido por Angular Material
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    ReactiveFormsModule
+    
   ],
   exports: [RouterModule]
 })
--- src/app/seccion-principal/segunda-seccion/estudios/estudios.component.ts
+++ src/app/seccion-principal/segunda-seccion/estudios/estudios.component.ts
@@ -73,70 +73,10 @@ export class EstudiosComponent implements OnInit {
     }
   }
 
-  /*logLibraryName() {
-    console.log(this.libraryName);
+  scrollToTop() {
+    // Desplazamiento suave hacia la parte superior
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-  
-
-  onFileChange(event: Event): void {
-    const file = (event.target as HTMLInputElement).files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        this.fileContent = e.target.result;
-        console.log('Archivo cargado correctamente:', this.fileContent); // Log de depuración
-      };
-      reader.readAsText(file);
-    } else {
-      console.error('No se seleccionó ningún archivo.'); // Log de error
-    }
-  }
-
-  importStudies(): void {
-    if (this.libraryName.trim() && this.fileContent) {
-      const lines = this.fileContent.split('\n');
-      const data = lines
-        .filter(line => line.includes('='))
-        .map(line => {
-          const [field, value] = line.split('=');
-          const cleanedValue = value.trim().replace(/[{}]|\,$/g, ''); // Eliminar llaves y coma al final
-          
-          return { field: field.trim(), value: cleanedValue };
-        });
-  
-      // Aquí separaremos los campos y valores por "DOI"
-      const groupedData: any = {};
-      data.forEach(item => {
-        const { field, value } = item;
-        
-        // Si el campo es DOI, no lo agrupamos (es una columna diferente)
-        if (field === 'DOI') {
-          groupedData[field] = value;
-        } else {
-          if (!groupedData[field]) {
-            groupedData[field] = [];
-          }
-          
-          // Si el valor es una lista de autores, dividimos por coma y agregamos en filas separadas
-          if (field === 'authors') {
-            const authors = value.split(',').map((author: string) => author.trim());
-            groupedData[field].push(...authors);
-          } else {
-            groupedData[field].push(value);
-          }
-        }
-      });
-  
-      // Convertimos el objeto agrupado en un arreglo para su visualización
-      this.importedData = Object.keys(groupedData).map(key => ({
-        field: key,
-        value: groupedData[key]
-      }));
-  
-    } else {
-      alert('Por favor, complete todos los campos y cargue un archivo válido.');
-    }
-  }*/
 
   /**
    * Inicia el flujo para seleccionar .bib o .ris
